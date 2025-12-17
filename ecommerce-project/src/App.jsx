7511c7252079d9a593d2594dded3baa673ea46ec
Kiril Kirilov <101038798+Kiril-Git@users.noreply.github.com>
2025-12-17 19:06:01 +0000

    Create multiple pages using rect-router

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router";
+import { HomePage } from "./pages/HomePage";
 import './App.css'
-import { HomePage } from "./pages/HomePage.jsx";
+
 
 function App() {
 
   return (
-    <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+          <Route path="checkout" element={<div>Test checkout page</div>} />
+      </Routes>
   )
 }
 
